f85b8ae190bc4bff5fa3027ed096100c1ea65f9d
Nate Flanagan <flanagnb@mail.uc.edu>
2024-06-24 00:56:37 -0400

    - Started adding Javascript section.

--- src/JavascriptExamples.jsx
+++ src/JavascriptExamples.jsx
@@ -1,3 +1,11 @@
 export default function JavascriptExamples() {
-  return <div id="joke-container"></div>
+  return (
+    <>
+      <div id="clocks">
+        <div id="analog-clock"></div>
+        <div id="digital-clock" style={{"font-family": "monospaced", "color": "green"}}></div>
+      </div>
+      <div id="joke-container"></div>
+    </>
+  )
 }
