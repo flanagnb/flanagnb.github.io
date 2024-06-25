99d5fcdaa91e50b5184fa27ab41871c4cee2d071
Nate Flanagan <flanagnb@mail.uc.edu>
2024-06-25 02:59:19 -0400

    - Updated joke container to be a react component

--- src/JavascriptExamples.jsx
+++ src/JavascriptExamples.jsx
@@ -16,14 +16,34 @@ function DigitalClock() {
   return <div id="digital-clock" style={{"font-family": "monospaced", "color": "green"}}>{time}</div>
 }
 
+function JokeContainer() {
+  const [joke, setJoke] = useState("loading a joke...");
+
+  function updateJoke() {
+    fetch("https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single")
+     .then(response => response.json())
+     .then(response_json => setJoke(response_json.joke))
+  }
+
+  useEffect(
+    () => {
+      let interval = setInterval(updateJoke, 60000);
+      return () => clearInterval(interval);
+    },
+    []
+  );
+
+  return <div id="joke-container">{joke}</div>
+}
+
 export default function JavascriptExamples() {
   return (
     <>
       <div id="clocks">
         <canvas id="analog-clock" width="150" height="150" style={{"background-color": "#999"}}></canvas>
-        <DigitalClock/> 
+        <DigitalClock/>
       </div>
-      <div id="joke-container"></div>
+      <JokeContainer/>
     </>
   )
 }
