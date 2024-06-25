095e63c081c2d19b7dfe18c0da7cc758bfe2867e
Nate Flanagan <flanagnb@mail.uc.edu>
2024-06-25 02:40:03 -0400

    - Pulled digital clock logic out to a react component.

--- src/JavascriptExamples.jsx
+++ src/JavascriptExamples.jsx
@@ -1,10 +1,27 @@
+import { useState, useEffect } from 'react';
+
+const timeFormatter = new Intl.DateTimeFormat("en-US", {"timeStyle": "short"});
+
+function DigitalClock() {
+  const [time, setTime] = useState(timeFormatter.format(new Date()));
+
+  useEffect(
+    () => {
+      let interval = setInterval(() => setTime(timeFormatter.format(new Date())), 60000);
+      return () => clearInterval(interval);
+    },
+    []
+  );
+
+  return <div id="digital-clock" style={{"font-family": "monospaced", "color": "green"}}>{time}</div>
+}
 
 export default function JavascriptExamples() {
   return (
     <>
       <div id="clocks">
         <canvas id="analog-clock" width="150" height="150" style={{"background-color": "#999"}}></canvas>
-        <div id="digital-clock" style={{"font-family": "monospaced", "color": "green"}}></div>
+        <DigitalClock/> 
       </div>
       <div id="joke-container"></div>
     </>
