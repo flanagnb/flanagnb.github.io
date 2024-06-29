9eb923cdf891faa7704565a5ddc334f3025ad7c2
Nate Flanagan <flanagnb@mail.uc.edu>
2024-06-28 21:21:13 -0400

    - Updated styling for bio page.

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,17 @@ import BioPage from './BioPage.jsx';
 import JavascriptExamples from './JavascriptExamples.jsx';
 import WelcomeBanner from './WelcomeBanner.jsx';
 
+import './App.css';
+
 export default function App() {
   return (
     <>
       <WelcomeBanner/>
-      <Tabs defaultActiveKey="bio" id="tabs">
-        <Tab eventKey="bio" title="Bio">
+      <Tabs defaultActiveKey="bio" id="tabs" justify fill>
+        <Tab eventKey="bio" title="Bio" className="tab">
           <BioPage/>
         </Tab>
-        <Tab eventKey="js" title="Javascript Examples">
+        <Tab eventKey="js" title="Javascript Examples" className="tab">
           <JavascriptExamples/>
         </Tab>
       </Tabs>
